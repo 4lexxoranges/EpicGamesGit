2eabd6d7eab6b9dcf3637963f89381a311f00232
4lexxoranges <or4nges.official@gmail.com>
2020-12-05 12:34:33 +0300

    Add game files

---
+++ Assets/Scripts/PauseControl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseControl : MonoBehaviour
+{
+    public GameObject pausePanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PauseOn()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            Time.timeScale = 0;
+            pausePanel.SetActive(true);
+        }
+    }
+    public void PauseOff()
+    {
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            Time.timeScale = 1;
+            pausePanel.SetActive(false);
+        }
+    }
+    public void Continue()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+    }
+    public void Menu()
+    {
+        Application.LoadLevel("MainMenu");
+    }
+
+    public void Settings()
+    {
+        Application.LoadLevel("Settings");
+    }
+}
---
+++ Assets/Scripts/SceneControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneControl : MonoBehaviour
+{
+    public void Start()
+    {
+        
+    }
+
+    public void Update()
+    {
+        
+    }
+    public void SceneManager(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
